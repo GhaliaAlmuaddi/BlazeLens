9474915b454e5b6f82d880f181575b4d80ef7df1
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-05-19 10:20:49 +0300

    Onboarding page - noti

---
+++ View/UserNotifications.swift
@@ -0,0 +1,18 @@
+//
+//  UserNotifications.swift
+//  CloudKidGameCenterTest
+//
+//  Created by Rahaf ALghuraibi on 11/11/1445 AH.
+//
+
+import SwiftUI
+
+struct UserNotifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserNotifications()
+}
